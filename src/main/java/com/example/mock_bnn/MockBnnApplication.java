04077be78eda29b36dfaed3895843bb021f3eaab
batmanovnn <batmanovnn@gmail.com>
2024-02-23 08:21:52 +0300

    My_first_mock.

---
+++ src/main/java/com/example/mock_bnn/MockBnnApplication.java
@@ -0,0 +1,13 @@
+package com.example.mock_bnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockBnnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockBnnApplication.class, args);
+    }
+
+}
